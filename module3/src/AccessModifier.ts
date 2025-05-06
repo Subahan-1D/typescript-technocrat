f13ff05854b6f38ac364039ba11cca2bb3deaff0
subahan ali <subahanislam523@gmail.com>
2025-05-06 11:57:19 +0600

    access modifiers

---
+++ module3/src/AccessModifier.ts
@@ -0,0 +1,29 @@
+{
+  // access Modifier
+
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    private _balance: number;
+
+    constructor(id: number, name: string, balance: number) {
+      (this.id = id), (this.name = name), (this._balance = balance);
+    }
+
+   public addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+
+   public getBalance() {
+      return this._balance
+    }
+  }
+
+  const lowAccount = new BankAccount(1, "Md.Subahan Ali", 1020);
+  lowAccount.addDeposit(24);
+  const myBalance = lowAccount.getBalance()
+  console.log(myBalance)
+  console.log(lowAccount);
+
+  
+}
